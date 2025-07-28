8228622ac263a6d277b44f1a8888b7faeaca30b3
Nacho Codoñer <igcogi@gmail.com>
2025-07-28 11:25:11 +0200

    refactor to describe a unique publish-reactive package

--- package.js
+++ package.js
@@ -1,12 +1,8 @@
 Package.describe({
   name: 'publish-reactive',
   version: '0.0.1',
-  // Brief, one-line summary of the package.
   summary: 'Reactive publish for Meteor with async support',
-  // URL to the Git repository containing the source code for this package.
   git: '',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
   documentation: 'README.md',
 });
 
