6a3ca2cbc0874c905cf9bea271118a7812b32f95
Nacho Codoñer <igcogi@gmail.com>
2025-09-14 12:17:37 +0200

    adjust Puppeteer launch timeout and add testing args

--- scripts/test-core.sh
+++ scripts/test-core.sh
@@ -66,9 +66,9 @@ if [[ ! -d "./meteor-core/dev_bundle/lib/node_modules/puppeteer" ]]; then
   cd "${curPWD}"
 fi
 
-# Modify puppeteer_runner.js to add protocolTimeout: 600_000 and additional args
-echo "Adding protocolTimeout: 600_000 and additional args to puppeteer.launch in puppeteer_runner.js"
-sed -i 's/headless: "new",/headless: "new",\n    protocolTimeout: 600_000,/' meteor-core/packages/test-in-console/puppeteer_runner.js
+# Modify puppeteer_runner.js to add protocolTimeout: 300_000 and additional args
+echo "Adding protocolTimeout: 300_000 and additional args to puppeteer.launch in puppeteer_runner.js"
+sed -i 's/headless: "new",/headless: "new",\n    protocolTimeout: 300_000,/' meteor-core/packages/test-in-console/puppeteer_runner.js
 sed -i "s/'--disable-web-security',/'--disable-web-security',\n      '--disable-dev-shm-usage',\n      '--disable-gpu',/" meteor-core/packages/test-in-console/puppeteer_runner.js
 
 # Test core
